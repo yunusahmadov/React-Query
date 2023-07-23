71667874b8808d7cb4c128c4875e414f0fef19b6
Yunus <yunus.ahmedov.11@mail.ru>
2023-07-23 20:12:06 +0400

    checking difference between default fetching and react query,added react query devtools

--- src/components/SuperHeroesPage.jsx
+++ src/components/SuperHeroesPage.jsx
@@ -1,20 +1,33 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+
+//Default Fetching
+
+
 function SuperHeroesPage() {
     const [isLoading,setIsLoading]=useState(true)
     const [data,setData]=useState([])
+    const [error,setError]=useState('')
 
         useEffect(() => {
         axios.get(' http://localhost:4000/superheroes').then((res)=>{
             setData(res.data)
             setIsLoading(false)
             console.log(res);
+        }).catch(error=>{
+          setError(error.message)
+          setIsLoading(false)
+
         })
         }, [])
         if (isLoading) {
             return <h2>Loading</h2>
         }
+        if (error) {
+          return <h2>{error}</h2>
+      }
+        
         
   return (
     <div>
